c09ab4cba9af1e6c3de68eac7073e8a1913b0cd3
Ashish sah <ashishsah1000@gmail.com>
2021-01-12 14:46:19 +0530

    Added the concept of event handeling We have to first declare the function variable in state and when we use the function then we have to call `this.state(function(data){return{ the thing you want to change}})`

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -6,6 +6,14 @@ class Example extends React.Component {
       count: 0
     };
     this.handleClick = this.handleClick.bind(this);
+    this.dClick =  this.dClick.bind(this);
+  }
+  dClick(){
+    this.setState((prevState)=>{
+      return{
+        count: prevState.count-1
+      }
+    })
   }
   handleClick() {
     this.setState(function (prevState) {
@@ -18,7 +26,8 @@ class Example extends React.Component {
     return (
       <div>
         <h1>Count : {this.state.count}</h1>
-        <button onClick={this.handleClick}>Click Me !</button>
+        <button onClick={this.handleClick}>Increment !</button> &nbsp;
+        <button onClick={this.dClick}>DEcrement !</button>
       </div>
     );
   }
